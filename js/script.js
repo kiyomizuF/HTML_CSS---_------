b0db5f73c055baf1abf3d719d8462a980a735be0
kiyomizuF <shimizu.itsuki@gmail.com>
2024-08-05 16:07:00 +0900

    カルーセル、a要素のhover、右下のTOPに戻るボタンを追加

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(function () {
+  // カルーセル
+  $(".carousel").slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    autoplaySpeed: 5000,
+    fade: true,
+    arrows: false,
+  });
+
+  // a要素を薄くする
+  $("a").on("mouseenter", function () {
+    $(this).animate(
+      {
+        opacity: 0.5,
+      },
+      500
+    );
+  });
+
+  $("a").on("mouseout", function () {
+    $(this).animate(
+      {
+        opacity: 1,
+      },
+      300
+    );
+  });
+
+  // スクロールでTOPに戻るボタンを表示する
+  $(window).scroll(function () {
+    const scrollValue = $(window).scrollTop();
+    if (scrollValue >= 100) {
+      $(".top-btn").fadeIn();
+    } else {
+      $(".top-btn").fadeOut();
+    }
+  });
+});
